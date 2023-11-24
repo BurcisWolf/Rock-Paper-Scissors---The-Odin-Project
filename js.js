d9edf50f2faf543590a3128025cedc8cf6de37aa
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2023-11-24 16:42:00 +0100

    Time to go

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 let computer = "";
-let player = "rock";
+let player = "scissors";
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -15,11 +15,23 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
     computerSelection = getComputerChoice();
-    console.log(computerSelection);
+    console.log("Computer: " + computerSelection);
     playerSelection = player;
-    console.log(playerSelection);
+    console.log("Player: " + playerSelection);
     if(playerSelection == computerSelection){
-        return console.log("You tied! Both of you played " +computerSelection);
+        return console.log("You tied!");
+    } else if(playerSelection == "rock" && computerSelection == "paper"){
+        return console.log("You lost, " + computerSelection + " wraps " + playerSelection);
+    } else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return console.log("You won, " + playerSelection + " beats " + computerSelection);
+    }  else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return console.log("You lost, " + computerSelection + " beats " + playerSelection);
+    } else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return console.log("You won, " + playerSelection + " cuts " + computerSelection);
+    } else if(playerSelection == "paper" && computerSelection == "rock"){
+        return console.log("You won, " + playerSelection + " wraps " + computerSelection);
+    } else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return console.log("You lost, " + computerSelection + " cuts " + playerSelection);
     }
 }
 
